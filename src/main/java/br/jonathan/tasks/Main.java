7d044e4f4f5ae2dbcefd96ca76c842d75d63bead
jonathanapm <jonathan.moraes6@gmail.com>
2023-01-07 11:29:44 -0300

    first commit

---
+++ src/main/java/br/jonathan/tasks/Main.java
@@ -0,0 +1,5 @@
+package br.jonathan.tasks;public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/br/jonathan/tasks/ApiTest.java
@@ -0,0 +1,2 @@
+package br.jonathan.tasks;public class ApiTest {
+}
